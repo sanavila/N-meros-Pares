09d296fbc46965099e532cafcdecbcb5f385be15
San Ávila <rute.leone@hotmail.com>
2022-12-02 08:34:19 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+let numeros = 100;
+
+function mostrarNumerosParesDeUmACem () {
+    for(i = 1; i <= numeros; i++) {
+        if(i % 2 == 0) {
+            console.log(i);
+        }
+    }
+}
+
+mostrarNumerosParesDeUmACem();
